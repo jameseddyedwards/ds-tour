cd1edb4abc8f2d017288e5a91cdebb43b952f13b
James 'Eddy' Edwards <james.edwards@datasift.com>
2013-07-26 17:42:44 +0100

    Further updates

--- js/global.js
+++ js/global.js
@@ -1,9 +1,14 @@
 $(function(){
 
-	$("a", "nav").click(function(){
+	var nav = $("a", "nav");
+
+	nav.click(function(){
 		var targetId = "#" + $(this).data("target"),
 			tabs = $(".tab");
 
+		nav.removeClass("active");
+		$(this).addClass("active");
+		
 		tabs.removeClass("active");
 		$(targetId).addClass("active");
 	});
