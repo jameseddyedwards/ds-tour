00401ca3d06077ec0a7fdb726e7ef2e622debd2f
James 'Eddy' Edwards <james.edwards@datasift.com>
2013-07-31 18:12:16 +0100

    Latest tour updates

--- js/global.js
+++ js/global.js
@@ -1,30 +1,45 @@
 $(function(){
 
+	// Navigation
 	var nav = $("a", "nav"),
-		userInfo = {
-			technical : null,
-			role : null,
-			industry : null
-		};
+		steps = $(".step");
 
-	nav.click(function(){
-		var targetId = "#" + $(this).data("target"),
+	nav.click(function() {
+		var targetId = $("#" + $(this).data("target")),
 			tabs = $(".tab");
 
 		nav.removeClass("active");
 		$(this).addClass("active");
 		
 		tabs.removeClass("active");
-		$(targetId).addClass("active");
+		targetId.addClass("active");
 	});
 
-	$("#start").click(function(){
+
+	// Start Click
+	$("#start").click(function() {
+		$("#info").hide();
+		$("#questions").show();
+	});
+
+	steps.click(function() {
+		var targetId = $("#" + $(this).data("target"));
+
+		steps.removeClass("active");
+
+		nav.removeClass("active");
+		$(this).addClass("active");
 		$("#info").hide();
 		$("#questions").show();
 	});
 
 	$(".button").click(function(event){
 		event.preventDefault();
+	});
+
+	$("#showme").click(function() {
+		$("#questions").hide();
+		$("#filters").show();
 	});
 
 });
